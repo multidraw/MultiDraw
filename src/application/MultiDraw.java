135c9a6defa2470c7a2c4dfd77dfa1a8153c424d
greg walker <ggwalker89@gmail.com>
2011-04-14 16:14:44 -0500

    final draft before meeting friday

--- src/application/MultiDraw.java
+++ src/application/MultiDraw.java
@@ -4,7 +4,12 @@ import items.OpenMenuItem;
 import items.SaveMenuItem;
 
 import java.awt.BorderLayout;
+import java.awt.ComponentOrientation;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -12,9 +17,13 @@ import java.net.URL;
 
 import javax.swing.ImageIcon;
 import javax.swing.JApplet;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
 import javax.swing.KeyStroke;
 
 import tools.EraserTool;
@@ -42,6 +51,9 @@ import controllers.ToolController;
 @SuppressWarnings("serial")
 public class MultiDraw extends JApplet {
 	
+	protected String userName;
+	protected String sessionName;
+		
 	protected DrawingCanvasView canvas;
 	protected ControlPanelView controlPanel;
 	protected ToolBarView toolBar;
@@ -69,11 +81,14 @@ public class MultiDraw extends JApplet {
 	 */
 	public void init() {
 		StartupScreen startup = new StartupScreen(this);
-		startup.login();
-		startup.chooseSession();
+		userName = startup.login();
+		sessionName = startup.chooseSession();
+		
+		
 		
 		setContentPane(new JTabbedPane());
 		
+		//Create Canvas Pane
 		JPanel canvasPane = new JPanel();
 		canvasPane.setLayout(new BorderLayout());
 		canvas = createDrawingCanvas();
@@ -88,8 +103,80 @@ public class MultiDraw extends JApplet {
 		menuBar = createMenuBarView(toolList, new FileMenuItemController( new OpenMenuItem(canvas), KeyStroke.getKeyStroke(KeyEvent.VK_O, KeyEvent.CTRL_DOWN_MASK)),
 				new FileMenuItemController(new SaveMenuItem(canvas), KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK)));
 		canvasPane.add(menuBar, BorderLayout.NORTH);
+		//End Create Canvas Pane
 		
+		//Create Session Pane
 		JPanel sessionPane = new JPanel();
+		sessionPane.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		
+		MenuBarView sessionMenuBar = createMenuBarView(toolList, new FileMenuItemController( new OpenMenuItem(canvas), KeyStroke.getKeyStroke(KeyEvent.VK_O, KeyEvent.CTRL_DOWN_MASK)),
+				new FileMenuItemController(new SaveMenuItem(canvas), KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK)));
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridy = 0;
+		c.gridx = 0;
+		c.gridwidth = 6;
+		sessionPane.add(sessionMenuBar, c);
+
+		JLabel sessionHost = new JLabel("Current Drawing Host: ");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weighty = 0.5;
+		c.gridy = 1;
+		c.gridx = 0;
+		c.weightx = 0.25;
+		c.gridwidth = 1;
+		sessionPane.add(sessionHost, c);
+		
+		JTextField hostName = new JTextField();
+		hostName.setEditable(false);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weighty = 0.5;
+		c.gridy = 1;
+		c.gridx = 4;
+		c.weightx = 0.75;
+		c.gridwidth = 1;
+		sessionPane.add(hostName, c);
+
+		JTextField sessionMembers = new JTextField();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weighty = 0.5;
+		c.weightx = 0.95;
+		c.gridy = 2;
+		c.gridx = 0;
+		c.gridwidth = 5;
+		c.ipady = 250;
+		sessionPane.add(sessionMembers, c);
+		
+		JScrollBar vbar = new JScrollBar(JScrollBar.VERTICAL);
+		vbar.setUnitIncrement(2);
+		vbar.setBlockIncrement(1);
+		vbar.addAdjustmentListener(new AdjustmentListener(){
+
+			@Override
+			public void adjustmentValueChanged(AdjustmentEvent e) {
+				//slides the list of sessions up and down
+			}
+			
+		});
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weighty = 0.5;
+		c.gridy = 2;
+		c.gridx = 5;
+		c.weightx = 0.05;
+		c.gridwidth = 1;
+		c.ipady = 250;
+		sessionPane.add(vbar, c);
+	
+		JButton changeDrawer = new JButton("Change Editor");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weighty = 0.5;
+		c.weightx = 0.0;
+		c.gridy = 3;
+		c.gridx = 0;
+		c.gridwidth = 1;
+		c.ipady = 0;
+		sessionPane.add(changeDrawer, c);
+		//End Create Session Pane
 		
 		JPanel toolDesignerPane = new JPanel();
 
@@ -231,7 +318,8 @@ public class MultiDraw extends JApplet {
 		frame.addWindowListener(new AppCloser());
 		frame.pack();
 		frame.setSize(600, 450);
-		frame.setMinimumSize(new Dimension(500, 350));
+		frame.setLocation(100, 100);
+		frame.setMinimumSize(new Dimension(600, 450));
 		frame.setVisible(true);
 	}
 
--- src/application/StartupScreen.java
+++ src/application/StartupScreen.java
@@ -1,9 +1,14 @@
 package application;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -14,6 +19,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JScrollBar;
 import javax.swing.JTextField;
 
 import application.MultiDraw.AppCloser;
@@ -24,14 +30,14 @@ public class StartupScreen {
 
 	public StartupScreen(MultiDraw multiDraw) {
 	}
-	public void login() {
+	public String login() {
 		state = 1;
 		final JFrame frame = new JFrame();
 		JPanel jPane = new JPanel();
 		jPane.setLayout(new BoxLayout(jPane, BoxLayout.Y_AXIS));
 		JLabel info = new JLabel("Please enter a username to use MultiDraw");
 		JLabel label = new JLabel("Username: ");
-		JTextField login = new JTextField();
+		final JTextField login = new JTextField();
 		JButton button = new JButton("Login");
 		
 		
@@ -67,10 +73,18 @@ public class StartupScreen {
 			
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				state = 2;
-				frame.removeAll();
-				frame.dispose();
-				frame.setVisible(false);
+				if(login.getText() == null || login.getText() == ""){
+					state = 1;
+					
+					
+				}
+				//else if(check to see is username is in use)
+				else{
+					state = 2;
+					frame.removeAll();
+					frame.dispose();
+					frame.setVisible(false);
+				}
 			}
 		});
 		
@@ -86,36 +100,80 @@ public class StartupScreen {
 		frame.getContentPane().setLayout(new BorderLayout());
 		frame.getContentPane().add(jPane, BorderLayout.CENTER);
 		frame.addWindowListener(new AppCloser());
+		frame.setLocation(100, 100);
 		frame.setSize(300, 200);
 		frame.setResizable(false);
 		frame.setVisible(true);
 		while(state == 1);
+		return login.getText(); 
 		
 		
 
 	}
 	
-	public void chooseSession() {
+	public String chooseSession() {
 		final JFrame frame = new JFrame();
 
 		JPanel jPane = new JPanel();
-		jPane.setLayout(new BoxLayout(jPane, BoxLayout.Y_AXIS));	
+		jPane.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
 		
 		JRadioButton createSession = new JRadioButton("Create New Session");
-		createSession.setAlignmentX(Component.CENTER_ALIGNMENT);
-		createSession.setPreferredSize(new Dimension(200, 20));
-		JRadioButton joinSession = new JRadioButton("Join Session");
-		joinSession.setAlignmentX(Component.CENTER_ALIGNMENT);
-		joinSession.setPreferredSize(new Dimension(200, 20));
-		JTextField sessions = new JTextField();
-		sessions.setPreferredSize(new Dimension(200,100));
-		sessions.setEditable(false);
-		JButton sessionChosen = new JButton("Select");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridy = 0;
+		c.gridx = 0;
+		c.gridwidth = 5;
+		jPane.add(createSession, c);
 
-		jPane.add(createSession);
-		jPane.add(joinSession);
-		jPane.add(sessions);
-		jPane.add(sessionChosen);
+		JRadioButton joinSession = new JRadioButton("Join Session");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weighty = 0.1;
+		c.gridy = 1;
+		c.gridx = 0;
+		c.gridwidth = 5;
+		jPane.add(joinSession, c);
+
+		JScrollBar vbar = new JScrollBar(JScrollBar.VERTICAL);
+		vbar.setUnitIncrement(2);
+		vbar.setBlockIncrement(1);
+		vbar.addAdjustmentListener(new AdjustmentListener(){
+
+			@Override
+			public void adjustmentValueChanged(AdjustmentEvent e) {
+				//slides the list of sessions up and down
+			}
+			
+		});
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weighty = 0.1;
+		c.gridy = 2;
+		c.gridx = 5;
+		c.weightx = 0.05;
+		c.gridwidth = 1;
+		c.ipady = 100;
+		jPane.add(vbar, c);
+
+		JTextField sessions = new JTextField();
+		sessions.setBackground(Color.WHITE);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weighty = 0.1;
+		c.gridy = 2;
+		c.gridx = 0;
+		c.weightx = 0.95;
+		c.gridwidth = 4;
+		c.ipady = 100;
+		jPane.add(sessions, c);
+
+		JButton sessionChosen = new JButton("Select");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weighty = 0.0;
+		c.gridy = 3;
+		c.gridx = 0;
+		c.weightx = 0.25;
+		c.ipady = 0;
+		c.gridwidth = 0;
+		
+		jPane.add(sessionChosen, c);
 		
 		ButtonGroup bg = new ButtonGroup();
 		bg.add(createSession);
@@ -161,11 +219,15 @@ public class StartupScreen {
 		frame.setTitle("Host/Join a Session");
 		frame.getContentPane().setLayout(new BorderLayout());
 		frame.getContentPane().add(jPane, BorderLayout.CENTER);
+		frame.addWindowListener(new AppCloser());
 		frame.validate();
-		frame.setLocation(400, 400);
-		frame.setSize(300, 300);
+		frame.setLocation(100, 100);
+		frame.setSize(250, 250);
 		frame.setVisible(true);
-		while(state == 2);
+		frame.setResizable(false);
+		frame.addWindowListener(new AppCloser());
 
+		while(state == 2);
+		return "session name";
 	}
 }

df86f3401a42c29c56e625c1aa8826bc6525aaa8
Derek Henscheid <derekhensheid@gmail.com>
2011-04-15 15:17:02 -0500

    commit

---
+++ src/utils/ServerUtil.java
@@ -0,0 +1,55 @@
+package utils;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.ArrayList;
+
+import rmi.server.MultiDrawServer;
+import tools.shapes.CanvasShape;
+
+public class ServerUtil {
+	
+	private static String session;
+	private static String userName;
+	private static ArrayList<CanvasShape> shapes;
+	
+	public static MultiDrawServer getServerInstance() {
+		Registry registry;
+		try {
+			registry = LocateRegistry.getRegistry("192.168.56.1", 1099);
+			return (MultiDrawServer) registry.lookup("MultiDrawServer");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} //TODO get server address;
+	}
+
+	public static String getSession() {
+		return session;
+	}
+
+	public static void setSession(String sessionIN) {
+		session = sessionIN;
+	}
+
+	public static String getUserName() {
+		return userName;
+	}
+
+	public static void setUserName(String userNameIN) {
+		userName = userNameIN;
+	}
+
+	public static ArrayList<CanvasShape> getShapes() {
+		return shapes;
+	}
+
+	public static void setShapes(ArrayList<CanvasShape> shapes) {
+		ServerUtil.shapes = shapes;
+	}
+	
+	
+	
+	
+}

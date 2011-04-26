681e25dbba0867e60a4bfab9ec60e91a0475cc92
Chris Barton <cbarton@inigral.com>
2011-04-25 23:43:23 -0500

    javadocs

--- src/rmi/Session.java
+++ src/rmi/Session.java
@@ -2,19 +2,30 @@ package rmi;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
 import tools.shapes.CanvasShape;
 
-
+/**
+ * Session maintains all of the state of the governed session.  In that it includes the current 
+ * drawer and all of the users.
+ *
+ */
 public class Session implements Serializable{
-
+	/**
+	 * String - The current name of the session.
+	 */
+	public String name;
+	
 	private static final long serialVersionUID = 1346464814726331606L;
 	private ArrayList<CanvasShape> shapes;
-	private ArrayList<String> activeUsers = new ArrayList<String>();
+	private ArrayList<String> activeUsers;
 	private String drawer;
-	public String name;
-
+	
+	/**
+	 * Constructor for the session creating it and adding the creator as the drawer.
+	 * @param userName - String The creator/current drawer of the session.
+	 */
 	public Session(String userName) {
+		activeUsers = new ArrayList<String>();
 		shapes = new ArrayList<CanvasShape>();
 		activeUsers.add(userName);
 		drawer = userName;
@@ -22,7 +33,6 @@ public class Session implements Serializable{
 	}
 	
 	/**
-	 * joinSession(String userName)
 	 * Joins the current session with the given username.
 	 * @param userName - String the user joining the session.
 	 * @return Session - this session.
@@ -32,10 +42,21 @@ public class Session implements Serializable{
 		return this;
 	}
 	
+	/**
+	 * Leaves the session for the given username.
+	 * @param userName - String The user that is leaving the session.
+	 */
 	public void leaveSession(String userName) {
 		activeUsers.remove(userName);
 	}
 	
+	/**
+	 * Determines if this session is empty or not.
+	 * @return - boolean 
+	 * 	<ul>
+	 *		<li>true - Session is empty.</li><li>false - Session has members.</li>
+	 *	</ul>
+	 */
 	public boolean isEmpty() {
 		return (activeUsers.size() == 0) ? true : false;
 	}
@@ -48,20 +69,28 @@ public class Session implements Serializable{
 		this.shapes = shapes;
 	}
 
+	/**
+	 * Gets all of the active users of the session.
+	 * @return ArrayList<String> The active users.
+	 */
 	public ArrayList<String> getActiveUsers() {
 		return activeUsers;
 	}
 
+	/**
+	 * Gets the current drawer of the session.
+	 * @return String The current drawer.
+	 */
 	public String getDrawer() {
 		return drawer;
 	}
 
+	/**
+	 * Sets the current drawer for this session.
+	 * @param drawer String The new drawer.
+	 */
 	public void setDrawer(String drawer) {
 		this.drawer = drawer;
-	}
-	
-	public List<CanvasShape> getObjects() {
-		return shapes;
 	}
 
 	/**
@@ -73,19 +102,24 @@ public class Session implements Serializable{
 		this.shapes = list;
 	}
 
+	/**
+	 * Adds an object to this session.
+	 * @param shape - CanvasShape The new shape to add.
+	 */
 	public void addObject(CanvasShape shape) {
 		if (containsObject(shape)) {
-			updateObject(shapes.indexOf(shape), shape);
+			shapes.set(shapes.indexOf(shape), shape);
 		} else {
 			shapes.add(0, shape);
 
 		}
 	}
 
-	public void updateObject(int index, CanvasShape shape) {
-		shapes.set(index, shape);
-	}
-
+	/**
+	 * Removes an object from this session.
+	 * @param shape - CanvasShape The shape to remove.
+	 * @return boolean - true if removed; false otherwise.
+	 */
 	public boolean removeObject(CanvasShape shape) {
 		if (containsObject(shape)) {
 			shapes.remove(shapes.indexOf(shape));
@@ -95,12 +129,12 @@ public class Session implements Serializable{
 		}
 	}
 	
+	/**
+	 * Determines if the given object is in the session.
+	 * @param shape - CanvasShape The shape to query over.
+	 * @return boolean - true if exists; false otherwise.
+	 */
 	public boolean containsObject(CanvasShape shape) {
 		return (shapes.indexOf(shape) != -1);
 	}
-
-	public CanvasShape getObject(int index) {
-		return shapes.get(index);
-	}
-
 }
--- src/utils/ServerUtil.java
+++ src/utils/ServerUtil.java
@@ -8,23 +8,37 @@ import rmi.client.MultiDrawClient;
 import rmi.server.MultiDrawServer;
 import tools.shapes.CanvasShape;
 
+/**
+ * The ServerUtil class maintains all of the "global" variables that the client will use within its
+ * lifetime.  It is the pure definition of a helper for the main program so that these variables do not
+ * need to be passed around the application.
+ *
+ */
 public class ServerUtil {
 
-	private Session session;
-	private String userName;
-	private ArrayList<CanvasShape> shapes;
-	private MultiDrawClient client;
-	private MultiDrawServer server;
+	private Session session;	// The current session of the client.
+	private String userName;	// The current username of the client.
+	private MultiDrawClient client;	// The current client impl object of the client.
+	private MultiDrawServer server;	// The current server object that the client interacts with.
+	private ArrayList<CanvasShape> shapes = new ArrayList<CanvasShape>();
 	
+	/**
+	 * Constructor for the utils class.
+	 * @param c - MultiDrawClient The current client.
+	 */
 	public ServerUtil(MultiDrawClient c){
 		client = c;
 	}
 
+	/**
+	 * Connects to the server if not connected already.
+	 * @return MultiDrawServer - The server object.
+	 */
 	public MultiDrawServer getServerInstance() {
 		try {
 			if ( server == null ){
-				//server = (MultiDrawServer)Naming.lookup("//cbarton.mine.nu:1099/MultiDrawServer");
-				server = (MultiDrawServer)Naming.lookup("//localhost:1099/MultiDrawServer");
+				server = (MultiDrawServer)Naming.lookup("//cbarton.mine.nu:1099/MultiDrawServer");
+				//server = (MultiDrawServer)Naming.lookup("//localhost:1099/MultiDrawServer");
 			} 
 			return server; 
 		} catch (Exception e) {
@@ -33,32 +47,56 @@ public class ServerUtil {
 		}
 	}
 
+	/**
+	 * Sets the current client impl object.
+	 * @param c - MultiDrawClient The client impl object.
+	 */
 	public void setClient(MultiDrawClient c) {
 		client = c;
 	}
 
+	/**
+	 * Gets the current client impl object.
+	 * @return MultiDrawClient - Client impl object.
+	 */
 	public MultiDrawClient getClient() {
 		return client;
 	}
 
+	/**
+	 * Gets the current session that the client is in.
+	 * @return Session - The current session.
+	 */
 	public Session getSession() {
 		return session;
 	}
 
+	/**
+	 * Sets the current session that the client is in.
+	 * @param sessionIN - Session the current session.
+	 */
 	public void setSession(Session sessionIN) {
 		session = sessionIN;
 	}
 
+	/**
+	 * Gets the current username of the client.
+	 * @return String - The current username.
+	 */
 	public String getUserName() {
 		return userName;
 	}
 
+	/**
+	 * Sets the current username of the client.
+	 * @param userNameIN - String the username.
+	 */
 	public void setUserName(String userNameIN) {
 		userName = userNameIN;
 	}
-
+	
 	public ArrayList<CanvasShape> getShapes() {
-		return shapes;
+		return shapes ;
 	}
 
 	public void setShapes(ArrayList<CanvasShape> shapes) {
